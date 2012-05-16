a5c17d3a12a87fcfb031dd0a4cef6acaeb120e6b
Philip Wills <philip.wills@guardian.co.uk>
2012-05-16 15:29:49 +0100

    Moving endpoint for heatmap into Api

--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -2,6 +2,7 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.concurrent._
 import net.liftweb.json._
 import lib.Backend
 import org.joda.time.DateTime
@@ -50,6 +51,21 @@ object Api extends Controller {
     }
   }
 
+  case class LinkCount(sel: String, hash: String, count: Int)
 
+  def linkCount(page: String, callback: Option[String] = None) = Action {
+    Async {
+      Backend.eventsFrom(page).asPromise map { events =>
+        withCallback(callback) {
+          val eventMap = events groupBy { e => (e.sel.getOrElse(""), e.hash.getOrElse("")) }
 
+          val linkCounts = for {
+            ((selector, hash), clicks) <- eventMap
+          } yield LinkCount(selector, hash, clicks.size)
+
+          Serialization.write(linkCounts.toList)
+        }
+      }
+    }
+  }
 }
