4642c7493f3061fb6c6109b37d3db3f1777ed1de
Philip Wills <philip.wills@guardian.co.uk>
2012-06-19 14:02:20 +0100

    Deleting dead code

--- app/lib/Calculator.scala
+++ app/lib/Calculator.scala
@@ -24,24 +24,6 @@ class Calculator(implicit sys: ActorSystem) {
     hitReports sendOff (_ => ESClickStreamFetcher.topPaths.toList)
     listsOfStuff sendOff (_.diff(hitReports.get(), cs))
   }
-
-  private def calcTopPaths(clickStream: ClickStream) = {
-    val totalClicks = clickStream.allClicks.size
-    val clicksPerPath = clickStream.allClicks.groupBy(_.path).map {
-      case (k, v) => (k, v, v.size)
-    }.toList
-    val topTen = clicksPerPath.sortBy(_._3).reverse
-
-    topTen map {
-      case (url, hits, hitCount) =>
-        HitReport(
-          url = url,
-          percent = hitCount.toDouble * 100 / totalClicks,
-          hits = hitCount,
-          hitsPerSec = (hitCount.toDouble / clickStream.secs) * Config.scalingFactor,
-          events = hits.toList)
-    }
-  }
 }
 
 case class ESPageView(dt: DateTime, url: String, queryString: String, documentReferrer: Option[String],
